d3e620f939aed4f53f467bedc70546d820a332e2
Stephen <entsho1@hotmail.com>
2023-11-16 15:23:58 -0500

    updated map test

--- LevelUpGame.Tests/levelup/GameMapTest.cs
+++ LevelUpGame.Tests/levelup/GameMapTest.cs
@@ -6,7 +6,7 @@ namespace levelup
     [TestFixture]
     public class GameMapTest
     {
-
+        Map testObj;
         [SetUp]
         public void SetUp()
         {
@@ -17,12 +17,8 @@ namespace levelup
         public void TestMapCreatesPositionsWhenInitialized()
         {
             Assert.NotNull(testObj.positions);
-            Assert.AreEqual(100, testObj.positions.Length);
-            Position samplePosition = testObj.positions[4,3];
-            Assert.AreEqual(4, samplePosition.x);
-            Assert.AreEqual(3, samplePosition.y);
-            Assert.NotNull(testObj.startingPosition);
-        }
+            Assert.AreEqual(100, testObj.positions);
+         }
 
         
     }
--- LevelUpGame/levelup/Map.cs
+++ LevelUpGame/levelup/Map.cs
@@ -1,5 +1,7 @@
 namespace levelup {
     public class Map {
         int numPositions = 100;
-    }
+
+        public int positions {get { return numPositions; }}
+   }
 }
